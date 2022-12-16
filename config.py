6714275c5a7bf8868add67cf8a0130b6b96e1f6a
rmata23 <95316129+rmata23@users.noreply.github.com>
2022-12-15 23:52:56 -0600

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ class Config(object):
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:pa$$w0rd@integradoradb.c0i2k8ilqa3a.us-east-1.rds.amazonaws.com/integradoradb'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:pa$$w0rd@integradoradb.ckolfxiqyp2y.us-east-1.rds.amazonaws.com/integradoradb'
 #    SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost/parkingmap'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
